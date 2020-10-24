f5959221b45f3261602313f1207490578e434070
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2020-10-24 22:39:55 +0000

    Add level-data.json contents.

--- app-engine/gophers/hello.go
+++ app-engine/gophers/hello.go
@@ -6,4 +6,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// This function's name
+// This function's name is a must. App Engine uses it to drive the requests properly.
+func init() {
+	// Starts a new Gin instance with no middle-ware
+	r := gin.New()
+
+	// Define
