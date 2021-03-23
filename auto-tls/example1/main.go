e4fce9b28fd6f4b54b03a181b7b4221d9fcf6731
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2021-03-23 07:29:04 +0000

    v0.10

--- auto-tls/example1/main.go
+++ auto-tls/example1/main.go
@@ -3,4 +3,13 @@ package main
 import (
 	"log"
 
-	"github.com/gin-gon
+	"github.com/gin-gonic/autotls"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Ping handler
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "p
