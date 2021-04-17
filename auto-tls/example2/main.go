2798dbb847f304696b1afab5ef73edebf4698049
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2021-04-17 18:06:29 +0000

    ```

--- auto-tls/example2/main.go
+++ auto-tls/example2/main.go
@@ -1,4 +1,15 @@
 package main
 
 import (
-	"
+	"log"
+
+	"github.com/gin-gonic/autotls"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/acme/autocert"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Ping handler
+	r.GET
