fc8724c79b363803e63f9890631ca9c5151773c1
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2021-05-20 06:59:41 +0000

    update to 2.0 version

--- basic/main.go
+++ basic/main.go
@@ -3,4 +3,14 @@ package main
 import (
 	"net/http"
 
-	"github.com/gin-gonic
+	"github.com/gin-gonic/gin"
+)
+
+var db = make(map[string]string)
+
+func setupRouter() *gin.Engine {
+	// Disable Console Color
+	// gin.DisableConsoleColor()
+	r := gin.Default()
+
+	// P
