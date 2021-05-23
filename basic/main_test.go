95ebfe2d195b481dceefc3352a487a1612508774
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2021-05-23 18:48:16 +0000

    add toggle

--- basic/main_test.go
+++ basic/main_test.go
@@ -1,1 +1,14 @@
-package m
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPingRoute(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.Ne
