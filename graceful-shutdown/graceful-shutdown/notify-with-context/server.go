d8cd9c780e39726c1103246e026f29414e130ab6
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2021-06-29 07:28:59 +0000

    cleaning tests

--- graceful-shutdown/graceful-shutdown/notify-with-context/server.go
+++ graceful-shutdown/graceful-shutdown/notify-with-context/server.go
@@ -1,1 +1,17 @@
-// build
+// build go1.16
+
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Create cont
