d0de73aa212ce015b51c265cfd5c29974a582d06
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2021-07-02 09:50:12 +0000

    add formula

--- graceful-shutdown/graceful-shutdown/notify-with-context/server.go
+++ graceful-shutdown/graceful-shutdown/notify-with-context/server.go
@@ -49,4 +49,8 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Serv
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
+}
---
+++ graceful-shutdown/graceful-shutdown/notify-without-context/server.go
@@ -0,0 +1,8 @@
+//go:build go1.8
+// +build go1.8
+
+package main
+
+import (
+	"context"
+	"log
