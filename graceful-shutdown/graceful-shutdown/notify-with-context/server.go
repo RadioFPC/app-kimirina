553081c0c9e935b8201661c4db75a45dc6a400d4
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2021-07-02 09:50:08 +0000

    Naming consistency

--- graceful-shutdown/graceful-shutdown/notify-with-context/server.go
+++ graceful-shutdown/graceful-shutdown/notify-with-context/server.go
@@ -45,4 +45,8 @@ func main() {
 	log.Println("shutting down gracefully, press Ctrl+C again to force")
 
 	// The context is used to inform the server it has 5 seconds to finish
-	// the request it is 
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Serv
