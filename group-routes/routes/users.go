a829322240316835242046d74c61449a93be8fd1
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2021-07-10 11:37:47 +0000

    Update about-us.md

--- group-routes/routes/users.go
+++ group-routes/routes/users.go
@@ -3,4 +3,13 @@ package routes
 import (
 	"net/http"
 
-	"github.com/gin-gonic/gin
+	"github.com/gin-gonic/gin"
+)
+
+func addUserRoutes(rg *gin.RouterGroup) {
+	users := rg.Group("/users")
+
+	users.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "users")
+	})
+	users.
