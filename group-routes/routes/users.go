8422182c85721b812f1321ca18725566671c23b7
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2021-07-10 22:58:51 +0000

    posix-cpu-timers: Fix nanosleep task_struct leak

--- group-routes/routes/users.go
+++ group-routes/routes/users.go
@@ -12,4 +12,8 @@ func addUserRoutes(rg *gin.RouterGroup) {
 	users.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, "users")
 	})
-	users.
+	users.GET("/comments", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "users comments")
+	})
+	users.GET("/pictures", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "users 
