6db3f935aa0c97a3f484a067fe22f4a8115e035f
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2021-07-14 11:07:16 +0000

    Version bump to 0.8.3

--- grpc/example1/gin/main.go
+++ grpc/example1/gin/main.go
@@ -3,4 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/htt
+	"net/http"
+
+	"github.com/gin-gonic/examples/grpc/example1/gen/helloworld/v1"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// Set 
