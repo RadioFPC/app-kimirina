df107fd8f00fc7d9dc5937c4cc06691bf9dba929
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2021-07-31 13:25:04 +0000

    Minor UI changes

--- ratelimiter/rate.go
+++ ratelimiter/rate.go
@@ -7,4 +7,15 @@ import (
 
 	"github.com/fatih/color"
 	"github.com/gin-gonic/gin"
-	"go.uber.o
+	"go.uber.org/ratelimit"
+)
+
+var (
+	limit ratelimit.Limiter
+	rps   = flag.Int("rps", 100, "request per second")
+)
+
+func init() {
+	log.SetFlags(0)
+	log.SetPrefix("[GIN] ")
+	log.SetOutput(
