424c07ab800641f11daefcea84d9dc556e5220c3
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2022-04-09 07:32:01 +0000

    drm/edid: add quirk for Medion MD30217PG

--- realtime-advanced/rooms.go
+++ realtime-advanced/rooms.go
@@ -2,4 +2,12 @@ package main
 
 import "github.com/dustin/go-broadcast"
 
-var roomChannels = make(map[string]broadca
+var roomChannels = make(map[string]broadcast.Broadcaster)
+
+func openListener(roomid string) chan interface{} {
+	listener := make(chan interface{})
+	room(roomid).Register(listener)
+	return listener
+}
+
+fun
