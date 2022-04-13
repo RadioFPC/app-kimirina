1dcf7feb4dc27499cd44ccd41a92036a6fa35e0f
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2022-04-13 18:58:20 +0000

    Update h11.md

--- realtime-advanced/routes.go
+++ realtime-advanced/routes.go
@@ -4,3 +4,14 @@ import (
 	"fmt"
 	"html"
 	"io"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func rateLimit(c *gin.Context) {
+	ip := c.ClientIP()
+	value := int(ips.Add(ip, 1))
+	if value%50 ==
