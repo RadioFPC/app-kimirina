407f8877bdcb6f52732855c549491f4c4b49fa0b
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2022-04-16 06:06:52 +0000

    SCSS

--- realtime-advanced/routes.go
+++ realtime-advanced/routes.go
@@ -14,4 +14,12 @@ import (
 func rateLimit(c *gin.Context) {
 	ip := c.ClientIP()
 	value := int(ips.Add(ip, 1))
-	if value%50 ==
+	if value%50 == 0 {
+		fmt.Printf("ip: %s, count: %d\n", ip, value)
+	}
+	if value >= 200 {
+		if value%200 == 0 {
+			fmt.Println("ip blocked")
+		}
+		c.Abort()
+		c.String(http.StatusS
