1434b45e509e924d2288923f6980d38c11d2d850
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2022-06-23 21:39:22 +0000

    Cambio menor

--- realtime-chat/main.go
+++ realtime-chat/main.go
@@ -18,4 +18,12 @@ func main() {
 
 	router.GET("/room/:roomid", roomGET)
 	router.POST("/room/:roomid", roomPOST)
-	router.DELETE("/r
+	router.DELETE("/room/:roomid", roomDELETE)
+	router.GET("/stream/:roomid", stream)
+
+	router.Run(":8080")
+}
+
+func stream(c *gin.Context) {
+	roomid := c.Param("roomid")
+	listene
