450e1e055c08ca704b1a140900dfb24d5cb84d7a
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2022-07-02 11:50:13 +0000

    More methods

--- realtime-chat/template.go
+++ realtime-chat/template.go
@@ -5,4 +5,6 @@ import "html/template"
 var html = template.Must(template.New("chat_room").Parse(`
 <html> 
 <head> 
-    <title>{{.r
+    <title>{{.roomid}}</title>
+    <link rel="stylesheet" type="text/css" href="http://meyerweb.com/eric/tools/css/reset/reset.css">
+    <script src="http://ajax.go
