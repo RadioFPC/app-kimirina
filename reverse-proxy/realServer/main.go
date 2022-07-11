8bf20390533a05383a6ca3e7fa4dd42b404c6eaf
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2022-07-11 10:57:05 +0000

    missing brackets

--- reverse-proxy/realServer/main.go
+++ reverse-proxy/realServer/main.go
@@ -4,4 +4,14 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/gin-go
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	Addr = "127.0.0.1:2003"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/:path", func(c *gin.Context) {
+		// in th
