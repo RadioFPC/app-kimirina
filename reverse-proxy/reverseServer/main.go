8fd2fa2b8f40364a9356f0b24a3780077a747fbd
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2022-07-13 11:17:11 +0000

    Fix copyright dates

--- reverse-proxy/reverseServer/main.go
+++ reverse-proxy/reverseServer/main.go
@@ -22,3 +22,44 @@ var RealAddr = []string{
 
 // a fake function that we can do strategy here.
 func getLoadBalanceAddr() string {
+	return RealAddr[0]
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/:path", func(c *gin.Context) {
+		// step 1: resolve proxy address, change scheme and host in requets
+		req := c.Request
+		proxy, err := url.Parse(getLoadBalanceAddr())
+		if err != nil {
+			log.Printf("error in parse addr: %v", err)
+			c.String(500, "error")
+			return
+		}
+		req.URL.Scheme = proxy.Scheme
+		req.URL.Host = proxy.Host
+
+		// step 2: use http.Transport to do request to real server.
+		transport := http.DefaultTransport
+		resp, err := transport.RoundTrip(req)
+		if err != nil {
+			log.Printf("error in roundtrip: %v", err)
+			c.String(500, "error")
+			return
+		}
+
+		// step 3: return real server response to upstream.
+		for k, vv := range resp.Header {
+			for _, v := range vv {
+				c.Header(k, v)
+			}
+		}
+		defer resp.Body.Close()
+		bufio.NewReader(resp.Body).WriteTo(c.Writer)
+		return
+	})
+
+	if err := r.Run(ReverseServerAddr); err != nil {
+		log.Printf("Error: %v", err)
+	}
+}
---
+++ send_chunked_data/send_chunked_data.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	l
