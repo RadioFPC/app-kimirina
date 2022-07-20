1369ab24eeb8350df2e3ae8b6b0760e3aa386479
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2022-07-20 13:30:12 +0000

    Words.

--- struct-lvl-validations/server.go
+++ struct-lvl-validations/server.go
@@ -3,4 +3,11 @@ package main
 import (
 	"net/http"
 
-	"github.c
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	validator "github.com/go-playground/validator/v10"
+)
+
+// User contains user information.
+type User struct {
+	FirstN
