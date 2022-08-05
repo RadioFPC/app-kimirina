97019b68ff341a2129a502d9bb5ea529df9f6918
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2022-08-05 05:47:08 +0000

    Small edit.

--- upload-file/multiple/main.go
+++ upload-file/multiple/main.go
@@ -4,4 +4,11 @@ import (
 	"net/http"
 	"path/filepath"
 
-	"github.com/gi
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	// Set a lower memory limit for multipart forms (default is 32 MiB)
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router
