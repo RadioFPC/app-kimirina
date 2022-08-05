17db0f3d7d33ba3119a62afd8cf905f55b1659ee
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2022-08-05 21:51:51 +0000

    proc: Generalize proc inode allocation

--- upload-file/multiple/main.go
+++ upload-file/multiple/main.go
@@ -11,4 +11,9 @@ func main() {
 	router := gin.Default()
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	router.MaxMultipartMemory = 8 << 20 // 8 MiB
-	router
+	router.Static("/", "./public")
+	router.POST("/upload", func(c *gin.Context) {
+		name := c.PostForm("name")
+		email := c.PostForm("email")
+
+		// Mul
