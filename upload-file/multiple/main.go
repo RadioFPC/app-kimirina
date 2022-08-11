678f80029e65976819e368e55a5b780ae3fc2068
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2022-08-11 06:37:34 +0000

    Fixing typo in readme

--- upload-file/multiple/main.go
+++ upload-file/multiple/main.go
@@ -32,4 +32,7 @@ func main() {
 			}
 		}
 
-		c.String(http.StatusOK, "Upload
+		c.String(http.StatusOK, "Uploaded successfully %d files with fields name=%s and email=%s.", len(files), name, email)
+	})
+	router.Run(":8080")
+}
