68117ff5cac97d7d3deaee6d02ae1f2257cc22b9
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2022-08-25 11:43:48 +0000

    Fixed tabbing

--- versioning/main.go
+++ versioning/main.go
@@ -9,4 +9,11 @@ import (
 func main() {
 	router := gin.Default()
 
-	// ver
+	// version 1
+	apiV1 := router.Group("/v1")
+
+	apiV1.GET("users", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "List Of V1 Users")
+	})
+
+	// User only can be added by authoriz
