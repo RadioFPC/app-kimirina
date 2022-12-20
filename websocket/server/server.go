c290c2ee9929cf4a698a91da0dbf2ce3c18e9527
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2022-12-20 12:33:10 +0000

    0.0.21

--- websocket/server/server.go
+++ websocket/server/server.go
@@ -11,4 +11,12 @@ import (
 
 var addr = flag.String("addr", ":8080", "http service address")
 
-var upgrader = websocket.Upgrader{} // use d
+var upgrader = websocket.Upgrader{} // use default option
+
+func echo(ctx *gin.Context) {
+	w,r := ctx.Writer, ctx.Request
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("upgrade:", err)
+		return
+	}
