7f39411c6077362fac6d8d6ea4bdf1a9402a72d1
RadioFPC <99588207+RadioFPC@users.noreply.github.com>
2022-12-20 08:03:03 +0000

    tools/hv: Check for read/write errors

--- websocket/server/server.go
+++ websocket/server/server.go
@@ -4,3 +4,11 @@ import (
 	"flag"
 	"log"
 	"text/template"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+var upgrader = websocket.Upgrader{} // use d
